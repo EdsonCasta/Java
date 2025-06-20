64e60ac120100034ab23942781a6dbde574b1a24
EdsonCasta <dedsoncasta07@gmail.com>
2025-06-20 12:04:47 -0500

    Relating series and episodes

--- screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/model/Episodio.java
+++ screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/model/Episodio.java
@@ -1,16 +1,27 @@
 package com.aluracursos.screenmatch.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
+@Entity
+@Table(name = "episodios")
+
 public class Episodio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private Integer temporada;
     private String titulo;
     private Integer numeroEpisodio;
     private double evaluacion;
     private LocalDate fechaDeLanzamiento;
+    @ManyToOne
     private Serie serie;
+
+    public Episodio(){}
 
     public Episodio(Integer numero, DatosEpisodio d) {
         this.temporada = numero;
