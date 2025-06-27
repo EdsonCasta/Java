c3a0e5e78baedfe60d723cf967d9159112146fa4
EdsonCasta <dedsoncasta07@gmail.com>
2025-06-27 16:09:40 -0500

    using spring web

--- screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -17,34 +17,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
+public class ScreenmatchApplication{
 
-	@Autowired
-	private SerieRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repository);
-		principal.mostrarElMenu();
-
-		/*
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obtenerDatos( "https://www.omdbapi.com/?t=game+of+thrones&apikey=2d783b4f");
-		System.out.println(json);
-
-		ConvierteDatos conversor = new ConvierteDatos();
-		var datos = conversor.obtenerDatos(json, DatosSerie.class);
-		System.out.println(datos);
-
-		json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&season=1&episode=1&apikey=2d783b4f");
-		DatosEpisodio episodio = conversor.obtenerDatos(json, DatosEpisodio.class);
-		System.out.println(episodio);
-		*/
-
-//		EjemploStreams ejemploStreams = new EjemploStreams();
-//		ejemploStreams.muestraEjamplo();
-	}
 }
+
