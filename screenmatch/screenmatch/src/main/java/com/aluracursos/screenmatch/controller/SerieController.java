806f6bbd9a10d7ff639dc369659faf625f04eeec
EdsonCasta <dedsoncasta07@gmail.com>
2025-07-02 15:21:37 -0500

    showing the top 5 series in the database

--- screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -20,4 +20,9 @@ public class SerieController {
     public List<SerieDTO> obtenerTodasLasSeries(){
         return service.obtenerTodasLasSeries();
     }
+
+    @GetMapping("/series/top5")
+    public List<SerieDTO> obtenerTop5(){
+        return service.obtenerTop5();
+    }
 }
