79d15cadc9dc11fba1addc8a309432333a0545bf
EdsonCasta <dedsoncasta07@gmail.com>
2025-07-02 11:42:01 -0500

    creating a service for series

--- screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/dto/SerieDTO.java
+++ screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/dto/SerieDTO.java
@@ -1,8 +1,6 @@
 package com.aluracursos.screenmatch.dto;
 
 import com.aluracursos.screenmatch.model.Categoria;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 
 public record SerieDTO (
         String titulo,
